340e2489f6bafdf44d2bd461c14cd3a1ca2d8915
gabrielsacht <gabrielsachtr@gmail.com>
2023-01-23 18:27:07 -0300

    requisitos iniciais (1, 2 e 3) do Login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { BrowserRouter, Redirect, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <BrowserRouter>
+      <Route exact path="/">
+        <Redirect to="/login" />
+      </Route>
+      <Route exact path="/login" component={ Login } />
+    </BrowserRouter>
   );
 }
 
